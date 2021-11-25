dffefd4e058bd1a1e310b883f70f7e5d4a466c69
Guruprasad Pandit <gppandit.gp@gmail.com>
2021-11-25 16:39:07 +0530

    Initial commit

---
+++ src/migrations/20211125110310-create-chat-message.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ChatMessages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      Message: {
+        type: Sequelize.TEXT
+      },
+      SenderID: {
+        type: Sequelize.INTEGER
+      },
+      RecipientID: {
+        type: Sequelize.INTEGER
+      },
+      IsDeleted: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ChatMessages');
+  }
+};
