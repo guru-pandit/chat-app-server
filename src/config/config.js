f74b41e5a3aedbf9ba3fe89f24d623644abf8e82
Guru Pandit <gppandit.gp@gmail.com>
2022-12-14 15:07:42 +0530

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -8,8 +8,8 @@ module.exports = {
     },
     test: {
         username: "root",
-        password: "Test123$$",
-        database: "chat_test",
+        password: null,
+        database: "database_test",
         host: "127.0.0.1",
         dialect: "mysql",
     },
